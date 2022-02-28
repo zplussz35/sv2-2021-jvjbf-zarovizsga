b88074651eaec3cc8c04a025b984e17a05318871
zplussz35 <15.zoltan.16@gmail.com>
2022-02-28 10:28:07 +0100

    SentenceTransformer elkészítése

---
+++ sv2-2021-jvjbf-zarovizsga/src/main/java/sentencetransformer/SentenceTransformer.java
@@ -0,0 +1,24 @@
+package sentencetransformer;
+
+public class SentenceTransformer {
+
+    public String shortenSentence(String sentence) {
+        Character firstChar = sentence.trim().charAt(0);
+        Character lastChar = sentence.trim().charAt(sentence.trim().length() - 1);
+
+        if (!Character.isAlphabetic(firstChar) || !(Character.toUpperCase(firstChar) == firstChar)) {
+            throw new IllegalArgumentException("Must start with capital letter!");
+        }
+
+        if (!lastChar.equals('!') && !lastChar.equals('?') && !lastChar.equals('.')) {
+            throw new IllegalArgumentException("Must end with . ! or ?");
+        }
+        String[] parts = sentence.trim().split(" ");
+        if (parts.length > 4) {
+            return parts[0] + " ... " + parts[parts.length - 1];
+        }
+        return sentence;
+    }
+
+
+}
